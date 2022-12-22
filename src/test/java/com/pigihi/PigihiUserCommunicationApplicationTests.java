33c562fc324038a90ec696302c892c56fbaf9b44
Ashish Sam <ashishsamtgeorge@mca.ajce.in>
2022-12-22 22:48:23 +0530

    Create directory structure

---
+++ src/test/java/com/pigihi/PigihiUserCommunicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pigihi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PigihiUserCommunicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
