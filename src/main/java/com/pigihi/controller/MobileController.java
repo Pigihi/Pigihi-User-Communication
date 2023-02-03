dc4219dfeef246775bea588009ef79d9f7b2781a
Ashish Sam T George <ashishsamtgeorge@mca.ajce.in>
2023-02-03 18:01:09 +0530

    Change API endpoint of email controller
    Add Eureka client properties in application.yml

--- src/main/java/com/pigihi/controller/MobileController.java
+++ src/main/java/com/pigihi/controller/MobileController.java
@@ -3,12 +3,19 @@
  */
 package com.pigihi.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Controller class for sending mobile messages
  * 
  * @author Ashish Sam T George
  *
  */
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/sendMobile")
 public class MobileController {
 
 }
